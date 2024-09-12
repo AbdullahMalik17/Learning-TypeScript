57a2a20de04bbdbdec68e7343cf25f35acc14d53
Abdullah Ather <abdullahather272@gmail.com>
2024-09-12 20:06:40 +0500

    Added Node Project

--- Node-Project/Calculator/app.js
+++ Node-Project/Calculator/app.js
@@ -1,14 +1,14 @@
-import { number } from "@inquirer/prompts";
+import { input } from "@inquirer/prompts";
 import { select } from "@inquirer/prompts";
 async function calculator() {
-    let num1 = await number({
+    let num1 = await input({
         message: "Enter First Number:",
         validate: function (value) {
             const isValid = !isNaN;
             return isValid || 'Please enter a valid number';
         }
     });
-    let num2 = await number({
+    let num2 = await input({
         message: "Enter Second Number:"
     });
     var operation = await select({
@@ -28,8 +28,19 @@ async function calculator() {
             }
         ]
     });
+    Number(num1);
+    Number(num2);
     if (operation === "Addition") {
-        console.log("The Addition of two number is ", num1 + num2);
+        console.log("The sum of two number is ", num1 + num2);
+    }
+    else if (operation === "Subtraction") {
+        console.log("The subtraction of two number is ", num1 - num2);
+    }
+    else if (operation === "Multiplication") {
+        console.log("The multiplication of two number is ", num1 * num2);
+    }
+    else {
+        console.log("The Divion of two number is ", num1 / num2);
     }
 }
 calculator();
